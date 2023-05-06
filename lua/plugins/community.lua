6cd227aa0cb3cbd82bef6b0170126dabf500774d
Rohan Godha <rgodha24@brophybroncos.org>
2023-05-06 13:42:30 -0700

    setup astronvim

---
+++ lua/plugins/community.lua
@@ -0,0 +1,12 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+}
